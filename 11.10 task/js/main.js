a042ca988fba6302a353075ecf46094226188117
ayanasgarli <6vzxdcg@code.edu.az>
2023-10-12 03:34:38 +0400

    task completed

---
+++ 11.10 task/js/main.js
@@ -0,0 +1,105 @@
+// JS - task 1
+
+// let num = Number(prompt("Enter a number"));
+
+// if (num < 0) {
+//     alert("Negative number");
+// } else if (num % 2 === 0) {
+//     alert("Positivr and even number");
+// } else {
+//     alert("Positive and odd number");
+// }
+
+// JS - task 2
+
+// let month = Number(window.prompt("Enter a number"));
+
+// switch(month){
+//     case 1:
+//         alert("January");
+//         break;
+//     case 2:
+//         alert("February");
+//         break;
+//     case 3:
+//         alert("March");
+//         break;
+//     case 4:
+//         alert("April");
+//         break;
+//     case 5:
+//         alert("May");
+//         break;
+//     case 6:
+//         alert("June");
+//         break;
+//     case 7:
+//         alert("July");
+//         break;
+//     case 8:
+//         alert("August");
+//         break;
+//     case 9:
+//         alert("September");
+//         break;
+//     case 10:
+//         alert("October");
+//         break;
+//     case 11:
+//         alert("November");
+//         break;
+//     case 12:
+//         alert("December");
+//         break;
+//     default:
+//         alert("invalid number")
+// }
+
+//JS - task 3
+
+// let  monthnum = prompt("Enter month number");
+
+// if (monthnum >= 1 &&  monthnum <= 12) {
+//     switch (monthnum) {
+//         case 3:
+//         case 4:
+//         case 5:
+//             alert("Spring");
+//             break;
+//         case 6:
+//         case 7:
+//         case 8:
+//             alert("Summer");
+//             break;
+//         case 9:
+//         case 10:
+//         case 11:
+//             alert("Autumn");
+//             break;
+//         case 12:
+//         case 1:
+//         case 2:
+//             alert("Winter");
+//             break;
+//     }
+// } else {
+//     alert("Invalid input");
+// }
+
+// JS - task 4
+
+// let num1 = Number(prompt("Enter the first number"));
+// let num2 = Number(prompt("Enter the second number"));
+// let num3 = Number(prompt("Enter the third number"));
+
+// if (num1 === num2 && num2 === num3) {
+//     alert("equals");
+// } else if (num1 >= num2 && num1 >= num3) {
+//     alert("num1");
+// } else if (num2 >= num1 && num2 >= num3) {
+//     alert("num2");
+// } else if (num3 >= num1 && num3 >= num2) {
+//     alert("num3");
+// }
+
+
