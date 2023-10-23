2074f53e3a5b90f0d0b9f711efe9809c44366787
ayanasgarli <6vzxdcg@code.edu.az>
2023-10-24 02:25:48 +0400

    task done

---
+++ 20.JS_DOM/app.js
@@ -0,0 +1,62 @@
+let container = document.createElement("div");
+container.style.textAlign = "center"; 
+document.body.appendChild(container);
+
+let box1 = document.createElement("div");
+box1.style.width = "960px";
+box1.style.height = "360px";
+box1.style.margin = "0 auto"
+box1.style.backgroundColor = "#333333";
+box1.style.textAlign = "center";
+box1.style.lineHeight = "360px";
+box1.style.color = "#fff";
+box1.style.fontFamily = "times new roman";
+box1.style.fontSize = "28px";
+box1.style.fontWeight = "600";
+box1.textContent = "960 x 360px";
+container.appendChild(box1);
+
+let box2 = document.createElement("div");
+box2.style.width = "290px";
+box2.style.height = "180px";
+box2.style.backgroundColor = "#333333";
+box2.style.textAlign = "center";
+box2.style.lineHeight = "180px";
+box2.style.color = "#fff";
+box2.style.margin = "20px 25px 0 0";
+box2.style.fontFamily = "times new roman";
+box2.style.fontSize = "20px";
+box2.style.display = "inline-block";
+box2.textContent = "290 x 180px";
+container.appendChild(box2);
+
+let box3 = document.createElement("div");
+box3.style.width = "290px";
+box3.style.height = "180px";
+box3.style.backgroundColor = "#333333";
+box3.style.textAlign = "center";
+box3.style.lineHeight = "180px";
+box3.style.color = "#fff";
+box3.style.margin = "20px 25px 0 0";
+box3.style.fontFamily = "times new roman";
+box3.style.fontSize = "20px";
+box3.style.display = "inline-block";
+box3.textContent = "290 x 180px";
+container.appendChild(box3);
+
+let box4 = document.createElement("div");
+box4.style.width = "290px";
+box4.style.height = "180px";
+box4.style.backgroundColor = "#333333";
+box4.style.textAlign = "center";
+box4.style.lineHeight = "180px";
+box4.style.color = "#fff";
+box4.style.margin = "20px 25px 0 0";
+box4.style.fontFamily = "times new roman";
+box4.style.fontSize = "20px";
+box4.style.display = "inline-block";
+box4.textContent = "290 x 180px";
+container.appendChild(box4);
+
+
+
